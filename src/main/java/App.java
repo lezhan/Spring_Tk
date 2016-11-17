e181f16b3b5c5936fa2062451e8185d1b52a87d1
lezhan <lezhan_spm@ukr.net>
2016-11-17 13:36:49 +0200

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+public class App {
+    private Client client;
+    private ConsoleEventLogger consoleEventLogger;
+
+    public void logEvent(String string){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ConsoleEventLogger.java
@@ -0,0 +1,3 @@
+public class ConsoleEventLogger {
+    public void logEvent(String string){}
+}
