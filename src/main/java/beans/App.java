9f7b1316e53a522ea0bafa5afb15c1a4152d68bb
lezhan <lezhan_spm@ukr.net>
2016-11-25 12:13:31 +0200

    enum

--- src/main/java/beans/App.java
+++ src/main/java/beans/App.java
@@ -27,17 +27,9 @@ public class App {
     }
 
     public static void main(String[] args) {
-//        Client client = new Client("1", "John Smith");
-//        EventLogger eventLogger = new ConsoleEventLogger();
-//        beans.App app = new beans.App(client, eventLogger);
-//        app.client = new Client("1", "John Smith");
-//        app.eventLogger = new ConsoleEventLogger();
-//        app.logEvent("Some event for user 1");
         ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
-//        Client client = (Client) ctx.getBean("client");
-//        EventLogger eventLogger = (EventLogger) ctx.getBean("eventLogger");
         App app = (App) ctx.getBean("app");
-//        Event event = (Event) ctx.getBean("event", Event.class);
+
         Event event = ctx.getBean( Event.class);
         app.logEvent(EventType.INFO, event, "Some event for 1");
 
